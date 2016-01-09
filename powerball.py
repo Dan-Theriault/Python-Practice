c04c98c77d1957bf770f6c0d7d3bcee010773a07
Daniel Theriault <dannymt97@gmail.com>
2016-01-09 17:00:44 -0500

    Quick-pick program for the powerball lottery

---
+++ powerball.py
@@ -0,0 +1,52 @@
+import random
+
+white_pot = range(59)
+for ball in white_pot:
+    ball += 1
+red_pot = range(35)
+for ball in red_pot:
+    ball += 1
+
+def draw():
+    nums = []
+    powerball = -1
+    white_pot_temp = list(white_pot) 
+    red_pot_temp = list(red_pot)
+    random.shuffle(white_pot_temp)
+    random.shuffle(red_pot_temp)
+    for j in range(5):
+        nums.append(white_pot_temp.pop())
+    powerball = red_pot_temp.pop()
+
+    temp_nums = []
+    for num in nums:
+        num = str(num)
+        if len(num) < 2:
+            num = '0'+num
+        temp_nums.append(num)
+    nums = temp_nums
+    del temp_nums
+
+    powerball = str(powerball)
+    if len(powerball) < 2:
+        powerball = '0'+powerball
+    return [nums, powerball]
+
+if __name__ == '__main__':
+    import sys
+    tickets = []
+    try:
+        for i in range(int(sys.argv[1])):
+            tickets.append(draw())
+    except(IndexError):
+        tickets.append(draw())
+
+    for tick in tickets:
+        for num in tick[0]:
+            print(num, end = ' ')
+        print(tick[1])
+
+
+        
+
+
