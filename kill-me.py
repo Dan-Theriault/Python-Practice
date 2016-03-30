b4c0be937d63b6cc3a0778495c1468b700229c63
Daniel Theriault <dannymt97@gmail.com>
2016-03-29 23:41:07 -0400

    added kill-me.py, a great way to test if your computer's fan works

---
+++ kill-me.py
@@ -0,0 +1,8 @@
+moments_of_suffering = 0
+sweet_release = 10e99
+an_enternity_in_pain = 10e-99
+
+# I aplogize in advance to anyone who runs this
+while moments_of_suffering < sweet_release:
+    print(moments_of_suffering)
+    moments_of_suffering += an_enternity_in_pain
